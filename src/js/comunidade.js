1d8f0969cbe95beb7439765560eff2edf8815b9d
Giovanna <giscalzone@gmail.com>
2026-05-15 20:27:30 -0300

    Feat: Adicionando JS no projeto para aparecer as comunidades

--- src/js/comunidade.js
+++ src/js/comunidade.js
@@ -9,3 +9,49 @@ function fecharAba(){
     document.getElementById("formulario").style.display = "none";
 
 }
+
+function criarComunidade(){
+
+    // PEGAR VALORES
+    let nome = document.getElementById("nomeComunidade").value;
+
+    let descricao = document.getElementById("descricao").value;
+
+    let categoria = document.getElementById("categoria").value;
+
+    let privacidade = document.getElementById("privacidade").value;
+
+    // CRIAR CARD
+    let card = document.createElement("div");
+
+    card.classList.add("cardComunidade");
+
+    // CONTEÚDO DO CARD
+    card.innerHTML = `
+        <h3>${nome}</h3>
+
+        <p>${descricao}</p>
+
+        <div class="infoComunidade">
+
+            <span>${categoria}</span>
+
+            <span>${privacidade}</span>
+
+        </div>
+    `;
+
+    // ADICIONAR CARD
+    document
+        .getElementById("listadeComunidades")
+        .appendChild(card);
+
+    // LIMPAR CAMPOS
+    document.getElementById("nomeComunidade").value = "";
+
+    document.getElementById("descricao").value = "";
+
+    // FECHAR FORMULÁRIO
+    fecharAba();
+
+}
