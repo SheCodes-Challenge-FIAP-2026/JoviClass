0655affe86798ecb3d1e0de702512e26615f873e
Amanda <amandalourenco.2512@gmail.com>
2026-05-16 00:08:26 -0300

    feat: Criação do js da pagina de materias

---
+++ src/js/materias.js
@@ -0,0 +1,32 @@
+const overlay = document.getElementById('overlay');
+const fecharModal = document.getElementById('fecharModal');
+const cancelar = document.getElementById('cancelar');
+const criarMateria = document.getElementById('criarMateria');
+const nomeMateria = document.getElementById('nomeMateria');
+const cardsMaterias = document.getElementById('cardsMaterias');
+
+
+function abrirFormulario(){
+    overlay.style.display = 'flex';
+}
+
+
+function fecharFormulario(){
+    overlay.style.display = 'none';
+}
+const overlay = document.getElementById('overlay');
+const fecharModal = document.getElementById('fecharModal');
+const cancelar = document.getElementById('cancelar');
+const criarMateria = document.getElementById('criarMateria');
+const nomeMateria = document.getElementById('nomeMateria');
+const cardsMaterias = document.getElementById('cardsMaterias');
+
+
+function abrirFormulario(){
+    overlay.style.display = 'flex';
+}
+
+
+function fecharFormulario(){
+    overlay.style.display = 'none';
+}
