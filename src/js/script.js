f198288a7f384f45072c86e5418eb97e6767ad02
Paloma <paloma.carmo@yahoo.com>
2026-05-15 23:55:35 -0300

    feat: Validação e criação do card

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,26 @@ openOverlayBtn.addEventListener("click", () => toggleOverlay(true));
 closeOverlayBtns.forEach(btn => {
   if (btn) btn.addEventListener("click", () => toggleOverlay(false));
 });
+
+
+criarMateria.addEventListener('click', () => {
+    const nome = nomeMateria.value.trim();
+
+    if(nome === ''){
+        alert('Digite um nome para a matéria');
+        return;
+    }
+
+    const card = document.createElement('a');
+    card.classList.add('cardMateria');
+    card.innerHTML = `
+        <img src="../assets/img/imgPasta.png" alt="Pasta">
+        <h3>${nome}</h3>
+        <p>Criada agora</p>
+        <button class="btnExcluir">Excluir</button>
+    `;
+
+    cardsMaterias.appendChild(card);
+    fecharFormulario();
+    alert(Matéria "${nome}" criada com sucesso!);
+});
